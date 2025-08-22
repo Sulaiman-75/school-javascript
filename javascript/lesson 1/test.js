cfd111c0fd7037f6f07ab457ecfe107271a4f48c
Sulaiman-75 <gooder123zxcgh@gmail.com>
2025-08-22 09:11:13 -0500

    Add files via upload

---
+++ javascript/lesson 1/test.js
@@ -0,0 +1,12 @@
+let sentence="my fav sport is "
+let sport="soccer"
+console.log(sentence + sport)
+console.log([sentenceence, sport].join(""))
+console.log(${sentence}${sport})
+//
+
+
+
+
+console.log();
+console.error(ddd)
---
+++ javascript/lesson 2/strings.js
@@ -0,0 +1,19 @@
+// primitive wrapper methods
+let firstName = "Sulaiman";
+let lastName = "Awan";
+let sentence = "My Name is Sulaiman";
+
+console.log(typeof firstName);
+
+//prints the letter of a string at a specified index
+console.log(firstName.charAt(5));
+//concatenates strings together
+console.log(firstName.concat(" ", lastName));
+//returns length of string (not a method)
+console.log(firstName.length)
+//checks whether a string ends with the string in the parenthesis
+console.log(sentence.endsWith(firstName));
+//returns the index of the given character if it does not exits returns -1
+console.log(firstName.indexOf("Sulaiman"));
+//returns the last index of the given character if it does not exits returns -1
+console.log(sentence.lastIndexOf("a"));
