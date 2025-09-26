c3a9ec3463caba0129e396aa3480d3b4f9f64472
Sulaiman Awan <gooder123zxcgh@gmail.com>
2025-09-26 09:03:38 -0500

    perimeters&areas

---
+++ javascript/lesson 2/classes.js
@@ -0,0 +1,27 @@
+/*
+Classes are templates for creating objects. They encapsulate data with code to work on that data.
+Classes are built on prototypes, but also have some syntax and semantics that are unique to them.
+Create a class with the class keyword.
+Classes use a constructor method for initializing new objects.
+*/
+
+class rectangle {
+    constructor(height, width,) {   
+        this.height = height;
+        this.width = width;
+    }
+         calcArea() {
+        return this.width * this.height;
+    }
+         calcPerimeter() {
+        return 2 * (this.width + this.height);
+    }
+    get area() {
+        return this.calcArea();
+    }
+    get perimeter() {
+        return this.calcPerimeter();
+    }
+}
+box = new rectangle(2, 5); 
+console.log(box.perimeter);
